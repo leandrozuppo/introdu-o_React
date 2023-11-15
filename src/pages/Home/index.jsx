d767fd1e80fdc7c0f902868ccfc11ff3c1c295c0
LEANDRO ZUPPO <leandro_zuppo@estudante.sesisenai.org.br>
2023-11-14 22:07:56 -0300

    Atualizaçao Figma

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+import img1 from "../../images/img1.png";
+
+export function Home(){
+  return(
+    <>
+      <img src={img1} alt="" />
+      <h1>Hello World!</h1>
+    </>
+  )
+}
